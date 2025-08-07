cbd28128ccd18b3b9e5d8f8e1e8b28bbf069ef12
Tech-with-anmol <anmollklfh@gmail.com>
2025-08-07 10:28:28 +0530

    worked on write tree

--- src/main.rs
+++ src/main.rs
@@ -8,10 +8,9 @@ use std::result::Result::Ok;
 use flate2::read::ZlibDecoder;
 use flate2::Compression;
 use flate2::write::ZlibEncoder;
+use sha1::Sha1;
 use std::path::Path;
 use std::io::Write;
-use sha1::{Sha1, Digest};
-use std::str;
 
 enum Command {
     Init,
@@ -25,6 +24,7 @@ enum Command {
     LsTree {
         tree_sha: String,
     },
+    WriteTree,
 }
 
 impl Command {
@@ -53,6 +53,12 @@ impl Command {
                     return Err("Usage: rgit ls-tree --name-only <tree_sha>".into());
                 }
                 Ok(Self::LsTree { tree_sha: args[3].clone()})
+            },
+            Some("write-tree") => {
+                if args.len() < 1 {
+                    return Err("Usage: rgit write-tree".into());
+                }
+                Ok(Self::WriteTree)
             },
             _ => Err(format!("Unknown command: {}", args[1])),
         }
@@ -158,6 +164,105 @@ impl Command {
                     i = sha_end;
                }
                
+            }
+
+            Self::WriteTree => {
+                let mut tree_entries = Vec::new();
+                for entry in fs::read_dir(".").unwrap() {
+                    tree_entries.sort_by_key(|entry| {
+                    entry.iter().skip_while(|&&b| b != b' ').skip(1).take_while(|&&b| b != 0).cloned().collect::<Vec<u8>>()
+                    });
+                    let entry = entry.unwrap();
+                    let path = entry.path();
+                    if path.is_file() {
+                        let content = fs::read(&path).unwrap_or_else(|_|{
+                            eprintln!("Error reading file: {}", path.display());
+                            std::process::exit(1);
+                        });
+                        let hasher_data = format!("blob {}\0{}", content.len(), String::from_utf8_lossy(&content));
+                        let mut hasher = Sha1::new();
+                        hasher.update(hasher_data.as_bytes());
+                        let hash = hasher.finalize();
+                        let hash_out = format!("{:x}", hash);
+                        let dir = &hash_out[0..2];
+                        let file_name = &hash_out[2..];
+                        let object_dir = Path::new(".git/objects").join(dir);
+                        let object_file = object_dir.join(file_name);
+                        fs::create_dir_all(&object_dir).unwrap_or_else(|_| {
+                            eprintln!("Error creating directory: {}", object_dir.display());
+                            std::process::exit(1);
+                        });
+                        let mut encoder = ZlibEncoder::new(Vec::new(), Compression::default());
+                        encoder.write_all(hasher_data.as_bytes()).unwrap_or_else(|_|{
+                            eprintln!("Error writing to zlib encoder");
+                            std::process::exit(1);
+                        });
+                        let compressed_data = encoder.finish();
+                        fs::write(&object_file, compressed_data.unwrap()).unwrap_or_else(|_| {
+                            eprintln!("Error writing to object file: {}", &object_file.display());
+                            std::process::exit(1);
+                        });
+                        let mode = "100644";
+                        let mut entry = Vec::new();
+                        entry.extend_from_slice(format!("{} {}\0", mode, filename).as_bytes());
+                        entry.extend_from_slice(&hex::decode(&hash_out).unwrap());
+                        tree_entries.push(entry);
+
+                    } else if path.is_dir() {
+                        let dir_name = entry.file_name().to_string_lossy();
+                        let mode = "40000";
+                        let sub_tree_hash = write_tree(&path); 
+                        let hasher_data = format!("40000 {}\0", dir_name).into_bytes();
+                        hasher_data.extend_from_slice(&hex::decode(&sub_tree_hash).unwrap());
+
+                        let mut hasher = Sha1::new();
+                        hasher.update(hasher_data.as_bytes());
+                        let hash = hasher.finalize();
+                        let hash_out = format!("{:x}", hash);
+                        let dir = &hash_out[0..2];
+                        let file_name = &hash_out[2..];
+                        let object_dir = Path::new(".git/objects").join(dir);
+                        let object_file = object_dir.join(file_name);
+                        fs::create_dir_all(&object_dir).unwrap_or_else(|_|{
+                            eprintln!("Error creating directory: {}", object_dir.display());
+                            std::process::exit(1);
+                        });
+                        let mut encoder = ZlibEncoder::new(Vec::new(), Compression::default());
+                        encoder.write_all(hasher_data.as_bytes()).unwrap_or_else(|_|{
+                            eprintln!("Error writing to zlib encoder");
+                            std::process::exit(1);
+
+                        });
+                        let compressed_data = encoder.finish();
+                        fs::write(&object_file, compressed_data.unwrap()).unwrap_or_else(|_|{
+                            eprintln!("Error writing to object file: {}", &object_file.display());
+                            std::process::exit(1);  
+                        });
+                        let entry_data =  format!("{} {}\0{}", mode, dir_name, hash_out);
+                        tree_entries.push(entry_data);
+
+                    } else {
+                        eprintln!("Unsupported file type: {}", path.display());
+                        std::process::exit(1);
+                    }
+
+
+                    }
+                let mut tree_content = Vec::new();
+                for entry in &tree_entries {
+                    tree_content.extend_from_slice(entry);
+                }
+                let header = format!("tree {}\0", tree_content.len());
+                let mut store = Vec::new();
+                store.extend_from_slice(header.as_bytes());
+                store.extend_from_slice(&tree_content);
+
+                let mut hasher = Sha1::new();
+                hasher.update(&store);
+                let tree_hash = hasher.finalize();
+                let tree_hash_hex = format!("{:x}", tree_hash);
+
+               }
             }
 
             
